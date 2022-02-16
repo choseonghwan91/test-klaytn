85b4be18f109d021b61842e4b6cd53dea91f66e8
seonghwancho <choseonghwan91@gmail.com>
2022-02-16 18:38:57 +0900

    test 파일

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import Caver from "caver-js";
 
-function App() {
+const App = () => {
+  const [account, setAccount] = useState();
+
+  // const caver = new Caver(window.klaytn);
+
+  function checkInstallKlaytn() {
+    if (typeof window.klaytn !== "undefined") {
+      console.log("실행할것");
+    } else {
+      alert("kaikas 설치가 되어 있지 않습니다.");
+    }
+  }
+
+  function getAccounts() {
+    window.klaytn.enable().then((res) => setAccount(res[0]));
+  }
+
+  function getBalanceValue() {
+    console.log(
+      window.klaytn.fromPeb(window.klaytn.getBalance({ account }), "KLAY")
+    );
+    // const balance = caver.klay.getBalance(account);
+    // console.log(balance);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>안녕하세요</div>
+      <div>{account}</div>
+      <button onClick={checkInstallKlaytn}>계정 활성화</button>
+      <button onClick={getAccounts}>계정 불러오기</button>
+      <button onClick={getBalanceValue}>잔액 불러오기</button>
     </div>
   );
-}
+};
 
 export default App;
